1229eff3f6987f2a17166068a906de127864f921
Oliver Voeltz <frechmatz@gmx.de>
2020-03-03 21:21:17 +0100

    Docstrings

--- src/html-writer.lisp
+++ src/html-writer.lisp
@@ -1,7 +1,7 @@
 (in-package :cl-readme)
 
 ;;
-;; DSL
+;; DSL helper functions
 ;;
 
 (defun heading-p (l)
@@ -26,56 +26,11 @@
   (getf (second l) :name))
 
 ;;
-;; TOC generation
+;; Rewriting
 ;;
 
-(defun generate-html-toc (output-stream doc)
-  "Traverse doc and generate a HTML list representing the TOC"
-  (labels ((sub-toc-p (sub-list)
-	     "Check if sub-list contains toc relevant headings.
-              Not a very efficient implementation as sub-tree may get parsed two times."
-	     (cond
-	       ((not (listp sub-list))
-		nil)
-	       ((toc-heading-p sub-list)
-		t)
-	       (t
-		(let ((found nil))
-		  (dolist (item sub-list)
-		    (if (sub-toc-p item)
-			(setf found t)))
-		  found))))
-	   (generate-html-toc-impl (sub-list)
-	     (cond
-	       ((not (listp sub-list))
-		nil)
-	       ((and (toc-heading-p sub-list) (sub-toc-p sub-list))
-		;; Current element is toc relevant and has toc-relevant sub elements
-		;; <li>...<ul>...</ul></li>
-		(format
-		 output-stream "<li><a href=\"#~a\">~a</a><ul>"
-		 (get-heading-id sub-list)
-		 (get-heading-name sub-list))
-		(dolist (item (rest (rest sub-list)))
-		  (generate-html-toc-impl item))
-		(format output-stream "</ul></li>"))
-	       ((and (toc-heading-p sub-list) (not (sub-toc-p sub-list)))
-		;; Current element is toc relevant and has no toc-relevant sub elements
-		;; <li>...</li>
-		(format
-		 output-stream "<li><a href=\"#~a\">~a</a></li>"
-		 (get-heading-id sub-list)
-		 (get-heading-name sub-list))
-		(dolist (item (rest (rest sub-list)))
-		  (generate-html-toc-impl item)))
-	       (t
-		(dolist (item sub-list)
-		  (generate-html-toc-impl item))))))
-    (format output-stream "<ul>")
-    (generate-html-toc-impl doc)
-    (format output-stream "</ul>")))
-
 (defun set-toc-heading-ids (doc)
+  "Assign ids to toc-headings"
   (let ((counter 0))
     (labels ((make-id (heading-settings)
 	       (setf counter (+ 1 counter))
@@ -99,21 +54,63 @@
                      (reverse c)))))
       (clone-list doc))))
 
+;;
+;; HTML Generation
+;;
 
-;;
-;;
-;; HTML-Writer
-;;
-;;
+(defun toc-to-html (output-stream doc)
+  "Traverse doc and generate a HTML list representing the TOC"
+  (labels ((sub-toc-p (sub-list)
+	     "Brute force implementation to check if a toc-heading contains sub toc-headings"
+	     (cond
+	       ((not (listp sub-list))
+		nil)
+	       ((toc-heading-p sub-list)
+		t)
+	       (t
+		(let ((found nil))
+		  (dolist (item sub-list)
+		    (if (sub-toc-p item)
+			(setf found t)))
+		  found))))
+	   (toc-to-html-impl (sub-list)
+	     (cond
+	       ((not (listp sub-list))
+		nil)
+	       ((and (toc-heading-p sub-list) (sub-toc-p sub-list))
+		;; Current element is a toc-heading and contains sub toc-headings
+		;; <li>...<ul>...</ul></li>
+		(format
+		 output-stream "<li><a href=\"#~a\">~a</a><ul>"
+		 (get-heading-id sub-list)
+		 (get-heading-name sub-list))
+		(dolist (item (rest (rest sub-list)))
+		  (toc-to-html-impl item))
+		(format output-stream "</ul></li>"))
+	       ((and (toc-heading-p sub-list) (not (sub-toc-p sub-list)))
+		;; Current element is a toc-heading that has no sub toc-headings
+		;; <li>...</li>
+		(format
+		 output-stream "<li><a href=\"#~a\">~a</a></li>"
+		 (get-heading-id sub-list)
+		 (get-heading-name sub-list))
+		(dolist (item (rest (rest sub-list)))
+		  (toc-to-html-impl item)))
+	       (t
+		(dolist (item sub-list)
+		  (toc-to-html-impl item))))))
+    (format output-stream "<ul>")
+    (toc-to-html-impl doc)
+    (format output-stream "</ul>")))
 
 (defun doc-to-html-internal (output-stream doc)
-  "Convert documentation to HTML"
+  "Traverse doc and generate HTML"
   (labels ((doc-to-html-impl (heading-level sub-list)
 	     (cond
 	       ((not (listp sub-list))
 		(format output-stream "~a" sub-list))
 	       ((toc-p sub-list)
-		(generate-html-toc output-stream doc))
+		(toc-to-html output-stream doc))
 	       ((semantic-p sub-list) ;; TODO Write class attribute if present
 		(format output-stream "<~a>" (get-semantic-name sub-list))
 		(dolist (item (rest (rest sub-list)))
@@ -122,7 +119,7 @@
 	       ((heading-p sub-list) ;; TODO Write class attribute if present
 		(format
 		 output-stream "<h~a~a>~a</h~a>"
-		 (+ 1 heading-level)
+		 (+ 1 heading-level) ;; TODO Take care of maximum HTML heading level
 		 (if (get-heading-id sub-list)
 		     (format nil " id=\"~a\"" (get-heading-id sub-list)) "")
 		 (get-heading-name sub-list)
@@ -136,8 +133,10 @@
     (doc-to-html-impl 0 doc)
     nil))
 
+;;
+;; API
+;;
+
 (defun doc-to-html (output-stream doc)
-  ;;(declare (optimize (debug 3) (speed 0) (space 0)))
-  (let ((rewritten-doc (set-toc-heading-ids doc)))
-    ;;(break)
-    (doc-to-html-internal output-stream rewritten-doc)))
+  "Convert documentation to HTML"
+  (doc-to-html-internal output-stream (set-toc-heading-ids doc)))
