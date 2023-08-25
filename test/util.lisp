ebcdb54c49a63c51a7c3e1b2b5e2d88f79538dd2
Oliver Voeltz <frechmatz@gmx.de>
2023-08-25 23:01:25 +0200

    Reworking unit tests

--- test/util.lisp
+++ test/util.lisp
@@ -1,8 +1,128 @@
 (in-package :cl-html-readme-test)
 
-(defun dsl-to-string (dsl)
-  "Evaluate DSL and return result as a string."
-  (let ((str (make-string-output-stream)))
-    (cl-html-readme:doc-to-html str dsl)
-    (get-output-stream-string str)))
+(defun dsl-to-string (doc)
+  "Deterministic stringification of an object following the syntax of the documentation DSL.
+   Uses the DSL tree walker, which is supposed to be tested.
+   In no means a generic list serializer."
+  (labels
+      ((format-item (item)
+	 (cond
+	   ((keywordp item)
+	    (format nil ":~a" (string-downcase (symbol-name item))))
+	   ((stringp item)
+	    (format nil "\"~a\"" item))
+	   ((numberp item)
+	    (format nil "~a" item))
+	   ((listp item)
+	    (error "DSL-Stringification: List not supported"))
+	   ((symbolp item)
+	    (format nil "~a" (string-downcase (symbol-name item))))
+	   ((not item)
+	    (format nil "NIL"))
+	   (t
+	    (format nil "t"))))
+       (get-plist-keys (plist)
+	 (let ((keys nil) (push-key t))
+	   (dolist (item plist)
+	     (if push-key
+		 (push item keys))
+	     (setf push-key (not push-key)))
+	   keys))
+       (get-plist-keys-sorted (plist)
+	 (sort
+	  (get-plist-keys plist)
+	  (lambda (a b)
+	    (string-lessp
+	     (format-item a)
+	     (format-item b))))))
+    (let ((buffer (make-string-output-stream)))
+      (cl-html-readme-dsl:walk-tree
+       doc
+       :open-element
+       (lambda(element-symbol element-properties content)
+	 (declare (ignore content))
+	 (format buffer " (~a (" (format-item element-symbol))
+	 (let ((first-keyword t))
+	   (dolist (key (get-plist-keys-sorted element-properties))
+	     (if (not first-keyword)
+		 (format buffer " "))
+	     (setf first-keyword nil)
+	     (format buffer "~a" (format-item key))
+	     (format buffer " ~a" (format-item (getf element-properties key)))))
+	 (format buffer ")"))
+       :close-element
+       (lambda(context)
+	 (declare (ignore context))
+	 (format buffer ")"))
+       :text
+       (lambda(str)
+	 (format buffer "~a" (format-item str))))
+      (get-output-stream-string buffer))))
 
+
+
+;;
+;; Test stuff
+;;
+
+(defun dsl-to-string-examples ()
+  (let ((examples
+	  (list
+	   ;; Not supported yet by tree-walker: plain string
+	   ;; (list
+	   ;;   :name "Plain string"
+	   ;;   :doc "Ollimaus")
+
+	   (list
+	    :name "A list containing a single string"
+	    :doc (list "Ollimaus"))
+
+	   ;; Not supported yet by tree-walker: Doc object is DSL special form
+	   ;; (list
+	   ;;  :name "A heading"
+	   ;;  :doc `(heading (:toc t :name "Heading 1")))
+	   
+	   (list
+	    :name "A string and then a DSL special form"
+	    :doc `("Intro" (heading (:toc t :name "Heading 1"))))
+
+	   (list
+	    :name "A string and then a DSL special form (alphabetically sorted properties)"
+	    :doc `("Intro" (heading (:d "D" :a "A" :z "Z"))))
+
+	   (list
+	    :name "A string and then a DSL special form (property type handling)"
+	    :doc `("Intro" (heading (:string "D" :number 123456 :keyword :a-keyword :boolean t))))
+	   
+	   ;; Not supported yet by tree-walker: Doc object is DSL special form
+	   ;; (list
+	   ;;  :name "A heading with embedded content"
+	   ;;  :doc `(heading (:toc t :name "Heading 1") "CONTENT"))
+
+	   ;; Not supported yet by tree-walker: Doc object is DSL special form
+	   ;; (list
+	   ;;  :name "A heading with follow-up content"
+	   ;;  :doc `((heading (:toc t :name "Heading 1")) "CONTENT"))
+
+	   ;; Not supported yet by tree-walker: Doc object is DSL special form
+	   ;; (list
+	   ;;  :name "Nested headings"
+	   ;;  :doc `((heading (:toc t :name "Heading 1")
+	   ;; 		    (heading (:name "Heading 1.1)") "CONTENT"))))
+
+	   ;; Not supported yet by tree-walker: Doc object is DSL special form
+	   ;; (list
+	   ;;  :name "A heading with id XXXXX"
+	   ;;  :doc `(heading (:toc t :id "XXXXX")))
+
+	   )))
+    (format t "~%~%Stringifying examples")
+    (dolist (example examples)
+      (format t "~%~%Formatting example: ~a" (getf example :name))
+      (format t "~%Output:~a" (dsl-to-string (getf example :doc))))
+    (format t "~%DONE~%")))
+
+    
+;;(dsl-to-string-examples)
+
+  
--- test/walk-tree-test.lisp
+++ test/walk-tree-test.lisp
@@ -1,141 +1,91 @@
 (in-package :cl-html-readme-test)
 
 (define-test walk-tree-test-1 ()
-	     (let ((doc '("STR-1" "STR-2" "STR-3"))
-		   (recorded nil))
-	       (cl-html-readme-dsl:walk-tree
-		doc
-		:open-element
-		(lambda(element properties content)
-		  (declare (ignore properties content))
-		  (push element recorded) nil) 
-		:close-element
-		(lambda(context)
-		  (declare (ignore context))
-		  nil)
-		:text
-		(lambda(str)
-		  (push str recorded)))
-	       (setf recorded (reverse recorded))
-	       (assert-equal "STR-1" (first recorded))
-	       (assert-equal "STR-2" (second recorded))
-	       (assert-equal "STR-3" (third recorded))))
-
+  (let ((doc '("STR-1" "STR-2" "STR-3")))
+    (let ((stringified (dsl-to-string '("STR-1" "STR-2" "STR-3"))))
+      (assert-equal "\"STR-1\"\"STR-2\"\"STR-3\"" stringified))))
 
 (define-test walk-tree-test-2 ()
-	     (let ((doc '("STR-1" (heading (:name "HEADING-1-NAME")) "STR-2" "STR-3"))
-		   (recorded nil))
-	       (cl-html-readme-dsl:walk-tree
-		doc
-		:open-element
-		(lambda(element properties content)
-		  (declare (ignore element content))
-		  (push "HEADING" recorded)
-		  (push (getf properties :name) recorded)
-		  nil) 
-		:close-element
-		(lambda(context)
-		  (declare (ignore context))
-		  nil)
-		:text
-		(lambda(str)
-		  (push str recorded)))
-	       (setf recorded (reverse recorded))
-	       (assert-equal "STR-1" (first recorded))
-	       (assert-equal "HEADING" (second recorded))
-	       (assert-equal "HEADING-1-NAME" (third recorded))
-	       (assert-equal "STR-2" (fourth recorded))
-	       (assert-equal "STR-3" (fifth recorded))))
+  (let ((doc '("STR-1" (heading (:name "HEADING-1-NAME")) "STR-2" "STR-3")))
+    (let ((stringified (dsl-to-string doc)))
+      (assert-equal
+       "\"STR-1\" (heading (:name \"HEADING-1-NAME\"))\"STR-2\"\"STR-3\""
+       stringified))))
 
+(define-test walk-tree-test-3-syntax-error ()
+  (let ((doc '("STR-1" nil))
+	(catched-error nil))
+    (handler-case
+	(cl-html-readme-dsl:walk-tree
+	 doc
+	 :open-element
+	 (lambda(element properties content)
+	   (declare (ignore element properties content))
+	   nil) 
+	 :close-element
+	 (lambda(context)
+	   (declare (ignore context))
+	   nil)
+	 :text
+	 (lambda(str)
+	   (declare (ignore str))
+	   nil))
+      (error (err)
+	(setf catched-error err)))
+    (assert-true catched-error)
+    (assert-true (typep catched-error 'cl-html-readme-dsl:dsl-syntax-error))))
 
-(define-test walk-tree-test-3 ()
-	     (let ((doc '("STR-1" nil))
-		   (catched-error nil))
-	       (handler-case
-		   (cl-html-readme-dsl:walk-tree
-		    doc
-		    :open-element
-		    (lambda(element properties content)
-		      (declare (ignore element properties content))
-		      nil) 
-		    :close-element
-		    (lambda(context)
-		      (declare (ignore context))
-		      nil)
-		    :text
-		    (lambda(str)
-		      (declare (ignore str))
-		      nil))
-		 (error (err)
-		   (setf catched-error err)))
-	       (assert-true catched-error)
-	       (assert-true (typep catched-error 'cl-html-readme-dsl:dsl-syntax-error))))
+(define-test walk-tree-test-4-syntax-error ()
+  (let ((doc '("STR-1" (mausi (:name "A")) "STR-2"))
+	(catched-error nil))
+    (handler-case
+	(cl-html-readme-dsl:walk-tree
+	 doc
+	 :open-element
+	 (lambda(element properties content)
+	   (declare (ignore element properties content))
+	   nil) 
+	 :close-element
+	 (lambda(context)
+	   (declare (ignore context))
+	   nil)
+	 :text
+	 (lambda(str)
+	   (declare (ignore str))
+	   nil))
+      (error (err)
+	(setf catched-error err)))
+    (assert-true catched-error)
+    (assert-true (typep catched-error 'cl-html-readme-dsl:dsl-syntax-error))))
 
-(define-test walk-tree-test-4 ()
-	     (let ((doc '("STR-1" (mausi (:name "A")) "STR-2"))
-		   (catched-error nil))
-	       (handler-case
-		   (cl-html-readme-dsl:walk-tree
-		    doc
-		    :open-element
-		    (lambda(element properties content)
-		      (declare (ignore element properties content))
-		      nil) 
-		    :close-element
-		    (lambda(context)
-		      (declare (ignore context))
-		      nil)
-		    :text
-		    (lambda(str)
-		      (declare (ignore str))
-		      nil))
-		 (error (err)
-		   (setf catched-error err)))
-	       (assert-true catched-error)
-	       (assert-true (typep catched-error 'cl-html-readme-dsl:dsl-syntax-error))))
-
-(define-test walk-tree-test-5 ()
-	     (let ((doc '("STR-1" (heading (:name "HEADING-1-NAME")) "STR-2"))
-		   (recorded nil))
-	       (cl-html-readme-dsl:walk-tree
-		doc
-		:open-element
-		(lambda(element properties content)
-		  (declare (ignore element content))
-		  (push "HEADING" recorded)
-		  (push (getf properties :name) recorded)
-		  "CLOSE-CONTEXT") 
-		:close-element
-		(lambda(context)
-		  (push context recorded)
-		  nil)
-		:text
-		(lambda(str)
-		  (push str recorded)))
-	       (setf recorded (reverse recorded))
-	       (assert-equal "STR-1" (first recorded))
-	       (assert-equal "HEADING" (second recorded))
-	       (assert-equal "HEADING-1-NAME" (third recorded))
-	       (assert-equal "CLOSE-CONTEXT" (fourth recorded))
-	       (assert-equal "STR-2" (fifth recorded))))
+(define-test walk-tree-test-5-close-element-context ()
+  (let ((doc '("STR-1" (heading (:name "HEADING-1-NAME")) "STR-2"))
+	(recorded nil))
+    (cl-html-readme-dsl:walk-tree
+     doc
+     :open-element
+     (lambda(element properties content)
+       (declare (ignore element content))
+       (push "HEADING" recorded)
+       (push (getf properties :name) recorded)
+       "CLOSE-CONTEXT") 
+     :close-element
+     (lambda(context)
+       (push context recorded)
+       nil)
+     :text
+     (lambda(str)
+       (push str recorded)))
+    (setf recorded (reverse recorded))
+    (assert-equal "STR-1" (first recorded))
+    (assert-equal "HEADING" (second recorded))
+    (assert-equal "HEADING-1-NAME" (third recorded))
+    (assert-equal "CLOSE-CONTEXT" (fourth recorded))
+    (assert-equal "STR-2" (fifth recorded))))
 
 (define-test walk-tree-test-6 ()
-	     (let ((doc '("STR-0" (heading (:name "HEADING-1-NAME") "STR-1.1" "STR-1.2")))
-		   (recorded nil))
-	       (cl-html-readme-dsl:walk-tree
-		doc
-		:open-element
-		(lambda(element properties content)
-		  (declare (ignore element properties))
-		  (push content recorded)
-		  nil) 
-		:close-element
-		(lambda(context) (declare (ignore context)) nil)
-		:text
-		(lambda(str) (declare (ignore str))))
-	       (let ((content (first recorded)))
-		 (assert-equal "STR-1.1" (first content))
-		 (assert-equal "STR-1.2" (second content)))))
-
-
-
+  (let ((doc '("STR-0" (heading (:name "HEADING-1-NAME") "STR-1.1" "STR-1.2"))))
+    (let ((stringified (dsl-to-string doc)))
+      (assert-equal
+       "\"STR-0\" (heading (:name \"HEADING-1-NAME\")\"STR-1.1\"\"STR-1.2\")"
+       stringified))))
