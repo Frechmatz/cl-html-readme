6aa65bb6b97c6b2b1b5c066973728e4f960b35bb
Oliver Voeltz <frechmatz@gmx.de>
2023-09-08 18:33:45 +0200

    Migrating tests to doc stringifier

--- test/extract-toc-test.lisp
+++ test/extract-toc-test.lisp
@@ -6,18 +6,15 @@
 			  (heading (:name "H2" :toc t))
 			  (heading (:name "H3" :toc t)))))
 	       (cl-html-readme-dsl:extract-toc doc tree-builder)
-	       (let ((toc (first (cl-html-readme-dsl:get-tree tree-builder))))
-		 ;; p ::= <properties>
-		 ;; toc ::= (toc-root p (toc-item p) (toc-item p) (toc-item p))
-		 (assert-true 5 (length toc))
-		 (assert-true (cl-html-readme-dsl:toc-root-p (first toc)))
-		 (let ((ti-1 (third toc)) (ti-2 (fourth toc)) (ti-3 (fifth toc)))
-		   (assert-true (cl-html-readme-dsl:toc-item-p (first ti-1)))
-		   (assert-equal "H1" (getf (second ti-1) :name))
-		   (assert-true (cl-html-readme-dsl:toc-item-p (first ti-2)))
-		   (assert-equal "H2" (getf (second ti-2) :name))
-		   (assert-true (cl-html-readme-dsl:toc-item-p (first ti-3)))
-		   (assert-equal "H3" (getf (second ti-3) :name))))))
+	       (let* ((toc (cl-html-readme-dsl:get-tree tree-builder))
+		      (expected-toc
+			'((toc-root (:class nil :style nil)
+			   (toc-item (:class nil :name "H1" :style nil :toc nil))
+			   (toc-item (:class nil :name "H2" :style nil :toc nil))
+			   (toc-item (:class nil :name "H3" :style nil :toc nil))))))
+		 (let ((toc-str (doc-to-string toc))
+		       (expected-toc-str (doc-to-string expected-toc)))
+		 (assert-equal expected-toc-str toc-str)))))
 
 (define-test test-extract-toc-2 ()
 	     (let ((tree-builder (make-instance 'cl-html-readme-dsl:tree-builder))
@@ -46,6 +43,28 @@
 		     (assert-equal "H2.2" (getf (second tc-2-ti-2) :name))
 		     (assert-true (cl-html-readme-dsl:toc-item-p (first ti-3)))
 		     (assert-equal "H3" (getf (second ti-3) :name)))))))
+
+(define-test test-extract-toc-2-2 ()
+	     (let ((tree-builder (make-instance 'cl-html-readme-dsl:tree-builder))
+		   (doc '((heading (:name "H1" :toc t))
+			  (heading (:name "H2" :toc t)
+			   (heading (:name "H2.1" :toc t))
+			   (heading (:name "H2.2" :toc t)))
+			  (heading (:name "H3" :toc t)))))
+	       (cl-html-readme-dsl:extract-toc doc tree-builder)
+	       (let* ((toc (cl-html-readme-dsl:get-tree tree-builder))
+		      (toc-str (doc-to-string toc))
+		      (expected-toc-str
+			(concatenate
+			 'string
+			 "((toc-root (:class nil :style nil) "
+			 "(toc-item (:class nil :name 'H1' :style nil :toc nil)) "
+			 "(toc-container (:class nil :container-class nil :container-style nil "
+			 ":name 'H2' :style nil :toc nil)"
+			 " (toc-item (:class nil :name 'H2.1' :style nil :toc nil))"
+			 " (toc-item (:class nil :name 'H2.2' :style nil :toc nil))) "
+			 "(toc-item (:class nil :name 'H3' :style nil :toc nil))))")))
+		 (assert-equal expected-toc-str toc-str ))))
 
 
 (define-test test-extract-toc-3 ()
