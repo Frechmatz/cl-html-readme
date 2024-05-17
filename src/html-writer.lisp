4b7321e951ad3e151a1e0a99746311bc75b2307c
Oliver Voeltz <frechmatz@gmx.de>
2024-05-17 22:37:10 +0200

    Reworked attribute rendering

--- src/html-writer.lisp
+++ src/html-writer.lisp
@@ -37,42 +37,70 @@
   "Private helper hook for unit tests to get more predictable HTML output.")
 
 ;;
-;; HTML generation
+;; Attribute rendering helper class
 ;;
 
-(defun format-custom-attributes-impl (attrs)
-  (let ((string-output-stream (make-string-output-stream))
-	(reading-key t)
-	(cur-key nil)
-	(cur-value nil))
-    (dolist (item attrs)
-      (if reading-key
-	  (setf cur-key item)
-	  (setf cur-value item))
-      (if (and (not reading-key) (stringp cur-value) (< 0 (length cur-value)))
+(defclass attribute-renderer ()
+  ((attributes :initform nil)))
+
+(defgeneric add-attribute (attribute-renderer indicator value))
+(defgeneric add-attributes (attribute-renderer plist))
+(defgeneric omit-attribute (attribute-renderer value))
+(defgeneric render-attributes (attribute-renderer &key prepend-space &allow-other-keys))
+
+(defmethod omit-attribute ((instance attribute-renderer) value)
+  (if (and (stringp value) (< 0 (length value)))
+      nil
+      t))
+  
+(defmethod add-attribute ((instance attribute-renderer) indicator value)
+  (let ((attribute-name
+	  (if (keywordp indicator)
+	      (string-downcase (symbol-name indicator))
+	      indicator)))
+    (with-slots (attributes) instance
+      (if (not (omit-attribute instance value))
+	  (push (list :name attribute-name :value value) attributes)))
+    nil))
+
+(defmethod add-attributes ((instance attribute-renderer) plist)
+  (cl-html-readme-plist-util:with-properties
+      plist
+      (lambda (key value)
+	(add-attribute instance (string-downcase (symbol-name key)) value)))
+  nil)
+
+(defmethod render-attributes ((instance attribute-renderer) &key prepend-space)
+  (let ((first-attr t) (string-output-stream (make-string-output-stream)))
+    (with-slots (attributes) instance
+      ;; follow insertion order
+      (dolist (attribute (reverse attributes))
+	(let ((name (getf attribute :name))
+	      (value (getf attribute :value)))
+	  (if (not first-attr)
+	      (format string-output-stream " "))
 	  (format
 	   string-output-stream
-	   " ~a=\"~a\""
-	   (string-downcase (symbol-name cur-key))
-	   cur-value))
-      (setf reading-key (not reading-key)))
-    (get-output-stream-string string-output-stream)))
+	   "~a=\"~a\"" name value))
+	(setf first-attr nil)))
+    (let ((rendered (get-output-stream-string string-output-stream)))
+      (if (and prepend-space (< 0 (length rendered)))
+	  (format nil " ~a" rendered)
+	  rendered))))
+
+;;
+;; HTML generation
+;;
 
 (defun serialize (output-stream doc)
   "Render documentation object. The object is supposed to follow the syntax of cl-html-readme-target-dsl."
   (labels ((newline ()
 	     (funcall *print-newline* output-stream))
-	   (format-id (properties)
-	     (if (getf properties :id)
-		 (format nil " id=\"~a\"" (getf properties :id)) ""))
 	   (format-heading (properties)
 	     (let ((level (getf properties :indentation-level)))
 	       (if (<= level 5)
 		   (format nil "h~a" (+ 1 level))
-		   (format nil "h6"))))
-	   (format-custom-attributes (fn properties)
-	     (format-custom-attributes-impl
-	      (funcall fn properties))))
+		   (format nil "h6")))))
     (let ((toc-properties nil))
       (cl-html-readme-base-dsl:walk
        (cl-html-readme-target-dsl:instance)
@@ -87,52 +115,76 @@
 	   ((string= "HEADING" (string-upcase (symbol-name form-symbol)))
 	    ;; <h{level} id={id} {render-hook}> {name} </h{level}>
 	    (newline)
-	    (format
-	     output-stream
-	     "<~a~a~a>~a</~a>"
-	     (format-heading form-properties)
-	     (format-id form-properties)
-	     (format-custom-attributes *get-heading-attributes* form-properties)
-	     (getf form-properties :name)
-	     (format-heading form-properties))
-	    nil)
+	    (let ((attribute-renderer (make-instance 'attribute-renderer)))
+	      (add-attribute attribute-renderer :id (getf form-properties :id))
+	      (add-attributes
+	       attribute-renderer
+	       (funcall
+		*get-heading-attributes*
+		form-properties))
+	      (format
+	       output-stream
+	       "<~a~a>~a</~a>"
+	       (format-heading form-properties)
+	       (render-attributes attribute-renderer :prepend-space t)
+	       (getf form-properties :name)
+	       (format-heading form-properties))
+	      nil))
 	   ;;
 	   ;; Semantic
 	   ;;
 	   ((string= "SEMANTIC" (string-upcase (symbol-name form-symbol)))
 	    (newline)
-	    ;; <{name {render-hook}}>...</{name}>
-	    (format
-	     output-stream
-	     "<~a~a>"
-	     (getf form-properties :name)
-	     (format-custom-attributes *get-semantic-attributes* form-properties))
-	    (format nil "</~a>" (getf form-properties :name)))
+	    (let ((attribute-renderer (make-instance 'attribute-renderer)))
+	      (add-attributes
+	       attribute-renderer
+	       (funcall
+		*get-semantic-attributes*
+		form-properties))
+	      ;; <{name {render-hook}}>...</{name}>
+	      (format
+	       output-stream
+	       "<~a~a>"
+	       (getf form-properties :name)
+	       (render-attributes attribute-renderer :prepend-space t))
+	      (format nil "</~a>" (getf form-properties :name))))
 	   ;;
 	   ;; Toc-Root
 	   ;;
 	   ((string= "TOC-ROOT" (string-upcase (symbol-name form-symbol)))
 	    (setf toc-properties form-properties)
 	    (newline)
-	    ;; <ul {render-hook}>...</ul>
-	    (format
-	     output-stream
-	     "<ul~a>"
-	     (format-custom-attributes *get-toc-root-attributes* toc-properties))
-	    "</ul>")
+	    (let ((attribute-renderer (make-instance 'attribute-renderer)))
+	      (add-attributes
+	       attribute-renderer
+	       (funcall
+		*get-toc-root-attributes*
+		toc-properties))
+	      ;; <ul {render-hook}>...</ul>
+	      (format
+	       output-stream
+	       "<ul~a>"
+	       (render-attributes attribute-renderer :prepend-space t)))
+	      "</ul>")
 	   ;;
 	   ;; Toc-Item
 	   ;;
 	   ((string= "TOC-ITEM" (string-upcase (symbol-name form-symbol)))
 	    ;; <li {render-hook}><a href=#{id}> {name} </a> </li>
 	    (newline)
-	    (format
-	     output-stream
-	     "<li~a><a href=\"#~a\">~a</a></li>"
-	     (format-custom-attributes *get-toc-item-attributes* toc-properties)
-	     (getf form-properties :id)
-	     (getf form-properties :name))
-	    nil)
+	    (let ((attribute-renderer (make-instance 'attribute-renderer)))
+	      (add-attributes
+	       attribute-renderer
+	       (funcall
+		*get-toc-item-attributes*
+		toc-properties))
+	      (format
+	       output-stream
+	       "<li~a><a href=\"#~a\">~a</a></li>"
+	       (render-attributes attribute-renderer :prepend-space t)
+	       (getf form-properties :id)
+	       (getf form-properties :name))
+	      nil))
 	   ;;
 	   ;; Toc-Container
 	   ;;
@@ -141,14 +193,26 @@
 	    ;; <ul {render-hook}>...</ul>
 	    ;; </li>
 	    (newline)
-	    (format
-	     output-stream
-	     "<li~a><a href=\"#~a\">~a</a><ul~a>"
-	     (format-custom-attributes *get-toc-item-attributes* toc-properties)
-	     (getf form-properties :id)
-	     (getf form-properties :name)
-	     (format-custom-attributes *get-toc-container-attributes* toc-properties))
-	    "</ul></li>")
+	    (let ((li-attribute-renderer (make-instance 'attribute-renderer))
+		  (ul-attribute-renderer (make-instance 'attribute-renderer))		  )
+	      (add-attributes
+	       li-attribute-renderer
+	       (funcall
+		*get-toc-item-attributes*
+		toc-properties))
+	      (add-attributes
+	       ul-attribute-renderer
+	       (funcall
+		*get-toc-container-attributes*
+		toc-properties))
+	      (format
+	       output-stream
+	       "<li~a><a href=\"#~a\">~a</a><ul~a>"
+	       (render-attributes li-attribute-renderer :prepend-space t)
+	       (getf form-properties :id)
+	       (getf form-properties :name)
+	       (render-attributes ul-attribute-renderer :prepend-space t))
+	      "</ul></li>"))
 	   (t (error (format nil "Dont know how to serialize ~a" form-symbol)))))
        :close-form-handler
        (lambda(context)
