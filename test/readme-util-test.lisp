748e323d14990fcff647096ff99822257c422919
Oliver Voeltz <frechmatz@gmx.de>
2024-06-28 20:19:56 +0200

    Bugfix: character entities must count as one char; Removed output-tab-width; Tests

--- test/readme-util-test.lisp
+++ test/readme-util-test.lisp
@@ -9,10 +9,9 @@
      &key
        replace-tabs
        escape
-       input-tab-width
-       output-tab-width)
+       input-tab-width)
   "Helper function to invoke string formatting with a very explicit interface"
-  (cl-html-readme::format-string-impl str replace-tabs escape input-tab-width output-tab-width))
+  (cl-html-readme::format-string-impl str replace-tabs escape input-tab-width))
 
 (defun test-format-string-make-input-string (items)
   "Build a string containing tabs and spaces where such characters are represented by keywords"
@@ -36,9 +35,9 @@
       (let ((ch (elt str i)))
 	(cond
 	  ((eql ch #\Tab)
-	   (write-string "<tab>" string-stream))
+	   (write-string "<t>" string-stream))
 	  ((eql ch #\Space)
-	   (write-string "<spc>" string-stream))
+	   (write-string "<s>" string-stream))
 	  (t (write-char ch string-stream)))))
     (get-output-stream-string string-stream)))
 
@@ -48,81 +47,87 @@
 
 (define-test test-format-string-plain ()
   "Plain string in and out"
-  (let* ((input-str (test-format-string-make-input-string (list "ABC")))
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list "ABC")))
 	 (output-str
 	   (test-format-string-format-output-string
 	    (test-format-string-invoke-format-string
 	     input-str
 	     :replace-tabs nil
 	     :escape nil
-	     :input-tab-width 4
-	     :output-tab-width 4))))
+	     :input-tab-width 4))))
     (assert-equal "ABC" output-str)))
 
 (define-test test-format-string-pass-through-tab-1 ()
   "Pass through a tab"
-  (let* ((input-str (test-format-string-make-input-string (list :tab "ABC")))
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list :tab "ABC")))
 	 (output-str
 	   (test-format-string-format-output-string
 	    (test-format-string-invoke-format-string
 	     input-str
 	     :replace-tabs nil
 	     :escape nil
-	     :input-tab-width 4
-	     :output-tab-width 4))))
-    (assert-equal "<tab>ABC" output-str)))
+	     :input-tab-width 4))))
+    (assert-equal "<t>ABC" output-str)))
 
 (define-test test-format-string-pass-through-tab-2 ()
   "Pass through multiple tabs"
-  (let* ((input-str (test-format-string-make-input-string (list :tab :tab "ABC")))
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list :tab :tab "ABC")))
 	 (output-str
 	   (test-format-string-format-output-string
 	    (test-format-string-invoke-format-string
 	     input-str
 	     :replace-tabs nil
 	     :escape nil
-	     :input-tab-width 4
-	     :output-tab-width 4))))
-    (assert-equal "<tab><tab>ABC" output-str)))
+	     :input-tab-width 4))))
+    (assert-equal "<t><t>ABC" output-str)))
 
 (define-test test-format-string-pass-through-tab-3 ()
   "Pass through a sequence of tabs and spaces"
-  (let* ((input-str (test-format-string-make-input-string (list :tab " " "ABC")))
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list :tab :space "ABC")))
 	 (output-str
 	   (test-format-string-format-output-string
 	    (test-format-string-invoke-format-string
 	     input-str
 	     :replace-tabs nil
 	     :escape nil
-	     :input-tab-width 4
-	     :output-tab-width 4))))
-    (assert-equal "<tab><spc>ABC" output-str)))
+	     :input-tab-width 4))))
+    (assert-equal "<t><s>ABC" output-str)))
 
 (define-test test-format-string-pass-through-tab-4 ()
   "Pass through a sequence of tabs and spaces"
-  (let* ((input-str (test-format-string-make-input-string (list " " :tab "ABC")))
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list :space :tab "ABC")))
 	 (output-str
 	   (test-format-string-format-output-string
 	    (test-format-string-invoke-format-string
 	     input-str
 	     :replace-tabs nil
 	     :escape nil
-	     :input-tab-width 4
-	     :output-tab-width 4))))
-    (assert-equal "<spc><tab>ABC" output-str)))
+	     :input-tab-width 4))))
+    (assert-equal "<s><t>ABC" output-str)))
 
 (define-test test-format-string-replace-tabs-1 ()
   "Replace a tab with spaces"
-  (let* ((input-str (test-format-string-make-input-string (list :tab "ABC")))
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list :tab "ABC")))
 	 (output-str
 	   (test-format-string-format-output-string
 	    (test-format-string-invoke-format-string
 	     input-str
 	     :replace-tabs t
 	     :escape nil
-	     :input-tab-width 4
-	     :output-tab-width 4))))
-    (assert-equal "<spc><spc><spc><spc>ABC" output-str)))
+	     :input-tab-width 4))))
+    (assert-equal "<s><s><s><s>ABC" output-str)))
 
 (define-test test-format-string-replace-tabs-2 ()
   "Replace a sequence of tabs and spaces with a sequence of spaces"
@@ -140,9 +145,8 @@
 	     input-str
 	     :replace-tabs t
 	     :escape nil
-	     :input-tab-width 4
-	     :output-tab-width 4))))
-    (assert-equal "<spc><spc><spc><spc><spc><spc><spc><spc>ABC" output-str)))
+	     :input-tab-width 4))))
+    (assert-equal "<s><s><s><s><s><s><s><s>ABC" output-str)))
 
 (define-test test-format-string-replace-tabs-3 ()
   "Replace a sequence of tabs and spaces with a sequence of spaces"
@@ -159,6 +163,79 @@
 	     input-str
 	     :replace-tabs t
 	     :escape nil
-	     :input-tab-width 4
-	     :output-tab-width 4))))
-    (assert-equal "<spc><spc><spc><spc>ABC" output-str)))
+	     :input-tab-width 4))))
+    (assert-equal "<s><s><s><s>ABC" output-str)))
+
+(define-test test-format-string-replace-tabs-4 ()
+  "Replace a sequence of tabs and spaces with a sequence of spaces"
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list
+	     "A"
+	     :space
+	     :tab
+	     "ABC")))
+	 (output-str
+	   (test-format-string-format-output-string
+	    (test-format-string-invoke-format-string
+	     input-str
+	     :replace-tabs t
+	     :escape nil
+	     :input-tab-width 4))))
+    (assert-equal "A<s><s><s>ABC" output-str)))
+
+(define-test test-format-string-replace-tabs-5 ()
+  "Replace special character with entity. Test that entity is counted as one character."
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list
+	     "&"
+	     :tab
+	     "ABC")))
+	 (output-str
+	   (test-format-string-format-output-string
+	    (test-format-string-invoke-format-string
+	     input-str
+	     :replace-tabs t
+	     :escape t
+	     ;; width greater than length of string representation of entity
+	     :input-tab-width 8))))
+    (assert-equal "&amp;<s><s><s><s><s><s><s>ABC" output-str)))
+
+(define-test test-format-string-replace-tabs-6 ()
+  "Replace special character with entity. Test that entity is counted as one character."
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list
+	     :space
+	     "&"
+	     :tab
+	     "ABC")))
+	 (output-str
+	   (test-format-string-format-output-string
+	    (test-format-string-invoke-format-string
+	     input-str
+	     :replace-tabs t
+	     :escape t
+	     ;; width greater than length of string representation of entity
+	     :input-tab-width 8))))
+    (assert-equal "<s>&amp;<s><s><s><s><s><s>ABC" output-str)))
+
+(define-test test-format-string-replace-tabs-7 ()
+  "Replace special character with entity. Test that entity is counted as one character."
+  (let* ((input-str
+	   (test-format-string-make-input-string
+	    (list
+	     :space
+	     "&&"
+	     :tab
+	     "ABC")))
+	 (output-str
+	   (test-format-string-format-output-string
+	    (test-format-string-invoke-format-string
+	     input-str
+	     :replace-tabs t
+	     :escape t
+	     ;; width greater than length of string representation of entity
+	     :input-tab-width 8))))
+    (assert-equal "<s>&amp;&amp;<s><s><s><s><s>ABC" output-str)))
